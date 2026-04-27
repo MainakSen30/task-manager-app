b6c1c6fd7fdfc931d2237014f777fae37dd752f7
MainakSen30 <senmainak2001@gmail.com>
2026-04-28 01:12:22 +0530

    database: connection established

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from src.utils.db import Base, engine
+from src.tasks.models import TaskModel
 
 Base.metadata.create_all(engine)
 app = FastAPI(title="Task Management API",
               description="API for managing tasks with CRUD operations"
               )
-
+print(engine.url)

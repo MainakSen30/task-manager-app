efd28de6ac0e0bed1aeca6aa4c4167675b2e60db
MainakSen30 <senmainak2001@gmail.com>
2026-05-03 13:52:41 +0530

    feature: get task by ID added

--- src/tasks/controller.py
+++ src/tasks/controller.py
@@ -1,6 +1,8 @@
 from src.tasks.dtos import TaskSchema as Task
 from sqlalchemy.orm import Session
 from src.tasks.models import TaskModel
+from fastapi import HTTPException
+
 def create_task(body: Task, db: Session):
     data = body.model_dump()
     # Here you would typically add the task to the database using the db session
@@ -22,4 +24,13 @@ def get_tasks(db: Session):
     return {
         "status": "All the data retrieved successfully",
         "data": tasks
+    }
+
+def get_task_by_id(task_id: int, db: Session):
+    one_task = db.query(TaskModel).get(task_id)
+    if not one_task:
+        raise HTTPException(404, detail = "Task id incorrect")
+    return {
+        "status": "task retrieved successfully",
+        "data": one_task
     }
--- src/tasks/router.py
+++ src/tasks/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from src.utils.db import get_db
 from src.tasks import controller
 from src.tasks.dtos import TaskSchema as Task
+
 task_routes =  APIRouter(prefix="/tasks")
 
 @task_routes.post("/create")
@@ -11,3 +12,7 @@ def create_task(body: Task, db = Depends(get_db)):
 @task_routes.get("/all_tasks")
 def get_all_tasks(db = Depends(get_db)):
     return controller.get_tasks(db)
+
+@task_routes.get("/one_task/{task_id}")
+def get_task_by_id(task_id: int, db = Depends(get_db)):
+    return controller.get_task_by_id(task_id, db)
